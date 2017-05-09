5973984bc48cdded124b8d4c6f4fece35bc9c189
kajsaffranzen <kajfr642@student.liu.se>
2017-05-09 10:33:22 +0200

    connected the search input to map

---
+++ app/components/SearchComponent.js
@@ -0,0 +1,28 @@
+import p from 'es6-promise';
+
+class SearchComponent {
+    constructor(){
+        this.input = '';
+        this.geocoder = new google.maps.Geocoder();
+    }
+    getCoordinates(){
+        this.input = document.getElementById('searchInput').value;
+
+        return new p.Promise( (res, rej) => {
+            this.geocoder.geocode({'address': this.input}, function(results, status) {
+                if (status === 'OK'){
+                    console.log('i ok');
+                    let obj = {
+                        city: results[0].formatted_address,
+                        lat: results[0].geometry.location.lat(),
+                        lng: results[0].geometry.location.lng()
+                    }
+                    res(obj);
+                }
+            })
+        })
+    }
+
+}
+
+export default SearchComponent;
--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
 import Map from './components/Mapbox'
+import Search from './components/SearchComponent';
 import AppContainer from './AppContainer.js'
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -6,19 +7,37 @@ var d3 = require('d3')
 
 var io = require('socket.io-client')('http://localhost:3000/');
 
-io.on('connect', function(data) {
+/*io.on('connect', function(data) {
     console.log('det här gick ju bra');
     io.emit('join', 'Hello World from client');
-})
-
-var a = ['h','he','hej'];
-var test = a.map(a => a.length);
+})*/
+document.getElementById('search-button').addEventListener("click", getCoord);
 
 let kajsa = new Map();
 var coords =  [18.082, 59.319];
 kajsa.centerMap(coords);
 
-ReactDOM.render(<AppContainer/>, document.getElementById('hello'));
+var search = new Search();
+function getCoord(){
+    var promise = search.getCoordinates();
+    promise.then(function(res){
+        centerMapbox(res);
+    })
+}
+
+function centerMapbox(obj){
+    var c = [obj.lng, obj.lat];
+    kajsa.centerMap(c);
+}
+
+var a = ['h','he','hej'];
+var test = a.map(a => a.length);
+
+
+
+
+
+/*ReactDOM.render(<AppContainer/>, document.getElementById('hello'));
 
 import SearchAction from './actions/SearchAction';
-SearchAction.addTodo('My first task');
+SearchAction.addTodo('My first task');*/
